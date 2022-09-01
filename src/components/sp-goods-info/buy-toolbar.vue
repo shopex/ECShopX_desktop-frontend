ba9090f5ea6d99e5254c1a122852ccd18fc7bb28
junzhe <2642581255@qq.com>
2022-09-01 10:09:07 +0800

    秒杀商品不可加入到购物车

--- src/components/sp-goods-info/buy-toolbar.vue
+++ src/components/sp-goods-info/buy-toolbar.vue
@@ -14,7 +14,7 @@
   .disabled {
     background-color: #cdcdcd;
     color: #fbfbfb;
-  } 
+  }
   .btn-addtocart {
     color: #fff;
   }
@@ -34,34 +34,51 @@ export default {
   },
   inheritAttrs: false,
   data() {
-    return {
-      
-    }
+    return {}
   },
   created() {},
   methods: {
     renderGiftBtn() {
-      return <div class='buy-toolbar'>
-        <div class='btn disabled'>赠品不可购买</div>
-      </div>
+      return (
+        <div class='buy-toolbar'>
+          <div class='btn disabled'>赠品不可购买</div>
+        </div>
+      )
     },
     renderInstockBtn() {
-      return <div class='buy-toolbar'>
-        <div class='btn disabled'>商品已下架</div>
-      </div>
+      return (
+        <div class='buy-toolbar'>
+          <div class='btn disabled'>商品已下架</div>
+        </div>
+      )
     },
     renderOnsaleBtn() {
-      const { theme } = this
-      return <div class='buy-toolbar'>
-        <div class='btn btn-addtocart' style={{
-          backgroundColor: theme,
-          borderColor: theme,
-        }} onClick={this.handleAddToCart}>加入购物车</div>
-        <div class='btn btn-fastbuy' style={{
-          borderColor: theme,
-          color: theme
-        }} onClick={this.handleFastBuy}>立即购买</div>
-      </div>
+      const { theme, info } = this
+      return (
+        <div class='buy-toolbar'>
+          <div
+            class='btn btn-addtocart'
+            style={{
+              display: info.activity_type != 'seckill' ? 'block' : 'none',
+              backgroundColor: theme,
+              borderColor: theme
+            }}
+            onClick={this.handleAddToCart}
+          >
+            加入购物车
+          </div>
+          <div
+            class='btn btn-fastbuy'
+            style={{
+              borderColor: theme,
+              color: theme
+            }}
+            onClick={this.handleFastBuy}
+          >
+            立即购买
+          </div>
+        </div>
+      )
     },
     handleAddToCart() {
       this.$emit('onAddToCart', 'cart')
@@ -72,14 +89,13 @@ export default {
   },
   render() {
     const { is_gift, approve_status } = this.info
-    if(is_gift) {
+    if (is_gift) {
       return this.renderGiftBtn()
-    } else if(approve_status == 'instock') {
+    } else if (approve_status == 'instock') {
       return this.renderInstockBtn()
-    } else if(approve_status == 'onsale') {
+    } else if (approve_status == 'onsale') {
       return this.renderOnsaleBtn()
     }
   }
 }
 </script>
-
