15cd5679169943147e016f52dd46912e74c57dfe
junzhe <2642581255@qq.com>
2022-09-23 11:14:48 +0800

    添加顶部菜单

---
+++ src/components/shop-header/index.vue
@@ -0,0 +1,56 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-03 14:27:08
+ * @LastEditTime: 2021-02-22 14:31:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /ecshopx-newpc/src/components/sp-header/index.vue
+-->
+<style lang="scss" src="./index.scss"></style>
+<template>
+  <div class="header-bar" >
+    <div class="header-bar__topbar">
+      <div class="container container-shop" >
+        <span>商家入驻</span>
+        <span class="exit-btn" @click="handleLogout()">退出</span>
+      </div>
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+import { mixin as clickaway } from '@/plugins/clickaway'
+import { mapState } from 'vuex'
+import { lockScreen } from '@/utils/dom'
+import S from '@/spx'
+
+export default {
+  name: 'ShopHeader',
+  mixins: [clickaway],
+  data() {
+    return {
+      message: '',
+      basefile: 'newwapmall/block/header.html',
+      fixed: false,
+      showSubNav: null,
+    }
+  },
+  methods: {
+    handleLogout() {
+      this.$cookies.remove('ECSHOPX_STORE_TOKEN')
+      S.logoutShop()
+    },
+    handleCallback(params) {
+      if (params) {
+        let keywords = params.data.keyword
+        this.$router.push(`/items?keywords=${keywords}`)
+      }
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
