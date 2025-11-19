559c4441e5d260a9dcba7b985f684d22917e307f
jerry <sunjingyu@shopex.cn>
2025-11-19 09:38:44 +0800

    fix:开源内容的标识修复

--- src/utils/language.js
+++ src/utils/language.js
@@ -1,20 +1,7 @@
-// +----------------------------------------------------------------------
-// | ECShopX open source E-commerce
-// | ECShopX 开源商城系统 
-// +----------------------------------------------------------------------
-// | Copyright (c) 2003-2025 ShopeX,Inc.All rights reserved.
-// +----------------------------------------------------------------------
-// | Corporate Website:  https://www.shopex.cn 
-// +----------------------------------------------------------------------
-// | Licensed under the Apache License, Version 2.0
-// | http://www.apache.org/licenses/LICENSE-2.0
-// +----------------------------------------------------------------------
-// | The removal of shopeX copyright information without authorization is prohibited.
-// | 未经授权不可去除shopeX商派相关版权
-// +----------------------------------------------------------------------
-// | Author: shopeX Team <mkt@shopex.cn>
-// | Contact: 400-821-3106
-// +----------------------------------------------------------------------
+/**
+ * Copyright © ShopeX （http://www.shopex.cn）. All rights reserved.
+ * See LICENSE file for license details.
+ */
 
 export const locales = [
     {
@@ -32,6 +19,5 @@ export const locales = [
   ]
   
   export const languageKey = 'i18n_redirected'
-  console.log('process.env.DEFAULT_LANG:', process.env.DEFAULT_LANG)
-  export const defaultLocale = process.env.DEFAULT_LANG || 'en'
+  export const defaultLocale = process.env.VUE_APP_DEFAULT_LANG || 'en'
   
